f835d3bf3b9539a0f34eae31c5498d70d66d025a
Gary.Yang <gary.yang@hy-tech.com.tw>
2020-08-05 09:54:09 +0800

    Updates

---
+++ precache-manifest.7efef322e672349d2f857092bb1d1f9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00eb300308d751052b9642d671bd08c4",
+    "url": "/resume.github.io/index.html"
+  },
+  {
+    "revision": "e3965143bdbab38e53a2",
+    "url": "/resume.github.io/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "59ca6c7634621a86600e",
+    "url": "/resume.github.io/static/css/main.a14e0653.chunk.css"
+  },
+  {
+    "revision": "e3965143bdbab38e53a2",
+    "url": "/resume.github.io/static/js/2.c252f7c8.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/resume.github.io/static/js/2.c252f7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ca6c7634621a86600e",
+    "url": "/resume.github.io/static/js/main.685d04ac.chunk.js"
+  },
+  {
+    "revision": "41320e076760b1c4d0dd",
+    "url": "/resume.github.io/static/js/runtime-main.dbd07778.js"
+  },
+  {
+    "revision": "94428a0ada1176de019a1ac2d135b447",
+    "url": "/resume.github.io/static/media/logo.94428a0a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.06911da33df1b1563be5f4e82a6412e5.js"
+  "/resume.github.io/precache-manifest.7efef322e672349d2f857092bb1d1f9c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
