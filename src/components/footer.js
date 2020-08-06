640c549eacab274d60cc7704c3c7b75fce1bba7f
Gary.Yang <gary.yang@hy-tech.com.tw>
2020-08-06 10:43:43 +0800

    1.增加瀏覽人數次數

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,7 +12,7 @@ class Footer extends React.Component {
                     <Container>
                         <Row style={{ 'padding': '25px 0 25px 0' }}>
                             <Col md={4} style={{ textAlign: 'center' }}>
-                                <img style={{ height: '80px' }} src={logo} />
+                                <img id="logo" style={{ height: '80px' }} src={logo} />
                             </Col>
                             <Col md={8} style={{ textAlign: 'left', margin: 'auto' }}>
                                 <p className="font_9 black" style={{ marginBottom: '0' }}>© 2020 by Gary Yang. Created by ReactJs Framework</p>
