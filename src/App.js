0a61d930c8e5e45522b6f5ee09c15846808825a6
Gary.Yang <gary.yang@hy-tech.com.tw>
2020-08-06 14:49:48 +0800

    ip Record

--- src/App.js
+++ src/App.js
@@ -15,9 +15,45 @@ import './css/animate.css/animate.min.css';
 import './css/animate.css/animate.css';
 import ScrollToTop from './components/scrollToTop';
 import Spinner from './components/spinner';
+import $ from 'jquery';
 
 function App(props) {
   const [loading, setLoading] = useState(true);
+  const [isCallIp, setIsCallIp] = useState(false);
+  React.useEffect(() => {
+    if (!isCallIp) {
+      setIsCallIp(true);
+      let ip = '';
+      let location = '';
+      let time = '';
+      let allThing = '';
+      $.ajax({
+        url: "https://www.cloudflare.com/cdn-cgi/trace",
+        success: function (response) {
+          allThing = response.split('=');
+          ip = allThing[3];
+          ip = ip.substring(0, ip.indexOf('\n'));
+          location = allThing[7];
+          location = location.substring(0, location.indexOf('\n'));
+          time = allThing[4];
+          time = time.substring(0, time.indexOf('\n'));
+          $.ajax({
+            url: "https://script.google.com/macros/s/AKfycbwezhMI0x6Ym5zHRDfeDWrg1zAxMhLcRlXcL8QPwQ/exec",
+            data: {
+                "ip": ip,
+                "location": location,
+                "time": time
+            },
+            success: function(response) {
+              if(response == "成功"){
+                console.log("Success to Record Ip");
+              }
+            },
+          });
+        },
+      });
+    }
+  })
   function setLoaded(loaded) {
     setLoading(loaded);
   }
